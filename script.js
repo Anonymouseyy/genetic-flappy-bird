be9b0205358395acdb791007d80863d8a54d54fe
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-07-06 15:47:03 -0700

    figuring out canvas stuff

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var sortingArea = {
+let gameArea = {
     canvas : document.getElementById("canv"),
     start : function() {
         this.canvas.width = document.documentElement.clientWidth * 0.9;
         this.canvas.height = document.documentElement.clientHeight * 0.8;
         this.context = this.canvas.getContext("2d");
-        this.interval = setInterval(updateCanvas, 20);
+        //this.interval = setInterval(updateCanvas, 20);
     },
     updateSize : function() {
         this.canvas.width = document.documentElement.clientWidth * 0.9;
@@ -15,8 +15,22 @@ var sortingArea = {
     }
 }
 
-function start() {
-    sortingArea.start();
+let player = {
+    x : 50,
+    y : 50,
+    radius: 50,
+    draw : function() {
+        const ctx = gameArea.context;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    }
 }
+
+function start() {
+    gameArea.start();
+    player.draw();
+}
+
+
 
 start();
