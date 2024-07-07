83991e18f83cbd9b444335e36d0628a9015f484e
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-07-06 21:56:14 -0700

    player working and background

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+function updateCanvas() {
+    gameArea.updateSize();
+
+    gameArea.context.rect(0, 0, gameArea.canvas.width, gameArea.canvas.height);
+    gameArea.context.fillStyle = "#3d87ff";
+    gameArea.context.fill();
+
+    player.update();
+    player.draw();
+}
+
 let gameArea = {
     canvas : document.getElementById("canv"),
     start : function() {
         this.canvas.width = document.documentElement.clientWidth * 0.9;
         this.canvas.height = document.documentElement.clientHeight * 0.8;
         this.context = this.canvas.getContext("2d");
-        //this.interval = setInterval(updateCanvas, 20);
+        this.interval = setInterval(updateCanvas, 20);
+        document.addEventListener("keydown", function (e) {
+            if (e.code === "Space") {
+                player.jump();
+            }
+        });
     },
     updateSize : function() {
         this.canvas.width = document.documentElement.clientWidth * 0.9;
@@ -16,21 +32,39 @@ let gameArea = {
 }
 
 let player = {
-    x : 50,
-    y : 50,
-    radius: 50,
+    x : 0.2,
+    y : 0.5,
+    yvel : 0,
+    radius: 0.075,
     draw : function() {
-        const ctx = gameArea.context;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    }
+        gameArea.context.beginPath();
+        gameArea.context.arc(this.x * gameArea.canvas.width, this.y * gameArea.canvas.height, this.radius * gameArea.canvas.height, 0, 2 * Math.PI);
+        gameArea.context.fillStyle = "#fff017";
+        gameArea.context.fill();
+        gameArea.context.stroke();
+    },
+    center : function() {
+        this.y = 0.5;
+        this.yvel = 0;
+    },
+    update : function() {
+        this.yvel += 0.000001;
+        this.y += this.yvel*gameArea.canvas.height;
+        
+        if (this.y < this.radius || this.y > 1-this.radius) {
+            this.center();
+        }
+    },
+    jump : function() {
+        this.yvel = -0.00002;
+    },
 }
 
 function start() {
     gameArea.start();
+    player.center();
     player.draw();
 }
-
 
 
 start();
